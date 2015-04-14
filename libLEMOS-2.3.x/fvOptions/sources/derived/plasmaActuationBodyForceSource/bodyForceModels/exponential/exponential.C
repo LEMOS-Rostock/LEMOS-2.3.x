bef7fea369eddf9d0c5994faa804f085fdeaeebc
Matthias Walter <matthias.walter@uni-rostock.de>
2015-04-14 23:18:19 +0200

    adding exponential body force

--- libLEMOS-2.3.x/fvOptions/sources/derived/plasmaActuationBodyForceSource/bodyForceModels/exponential/exponential.C
+++ libLEMOS-2.3.x/fvOptions/sources/derived/plasmaActuationBodyForceSource/bodyForceModels/exponential/exponential.C
@@ -29,7 +29,6 @@ License
 #include "DimensionedField.H"
 #include "fvMatrices.H"
 
-using namespace Foam::constant;
 
 // * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
 
@@ -41,15 +40,23 @@ namespace Foam
 }
 
 
-Foam::scalar Foam::exponential::calcAxialLength() const
+void Foam::exponential::calcForceField()
 {
+  scalar magD=mag(D_);
+
+  if (magD < SMALL)
+  {
+      WarningIn("Foam::fv::plasmaActuatorBodyForce::exponential::calcAxialLength() const")
+          <<"D vector has zero length!"<<endl;
+  }
+
+  vector nD = D_/magD;
+
   vector min = boundBox(UIndirectList<vector>(mesh_.C(), cells_)()).min();
   vector max = boundBox(UIndirectList<vector>(mesh_.C(), cells_)()).max();
 
   vectorField cellCenters(UIndirectList<vector>(mesh_.C(), cells_)());
 
-  tmp<vectorField> bodyForce(new vectorField(cells_.size(), vector::zero));
- 
   forAll(cellCenters, cellI)
   {
       const vector& cf = cellCenters[cellI];
@@ -59,27 +66,12 @@ Foam::scalar Foam::exponential::calcAxialLength() const
       tcf.y() =  (min.y() - cf.y())/ (min.y() - max.y());
       tcf.z() =  (min.z() - cf.z())/ (min.z() - max.z());
 
+      scalar X = (Cx_[0]+Cx_[1]*tcf.x()+Cx_[2]*sqr(tcf.x()))*exp(-1.0*Cx_[3]*pow(tcf.x(), Cx_[4]));
+      scalar Y = (Cy_[0]+Cy_[1]*tcf.y()+Cy_[2]*sqr(tcf.y()))*exp(-1.0*Cy_[3]*pow(tcf.y(), Cy_[4]));
+      scalar Z = (Cz_[0]+Cz_[1]*tcf.z()+Cz_[2]*sqr(tcf.z()))*exp(-1.0*Cz_[3]*pow(tcf.z(), Cz_[4]));
 
-      bodyForce()[cellI].x() = fMax_.x() * (Cx_[0]+Cx_[1]*tcf.x()+Cx_[2]*sqr(tcf.x()))*exp(-1.0*Cx_[3]*pow(tcf.x(), Cx_[4]));
-      bodyForce()[cellI].y() = fMax_.y() * (Cy_[0]+Cy_[1]*tcf.y()+Cy_[2]*sqr(tcf.y()))*exp(-1.0*Cy_[3]*pow(tcf.y(), Cy_[4]));
-      bodyForce()[cellI].z() = fMax_.z() * (Cz_[0]+Cz_[1]*tcf.z()+Cz_[2]*sqr(tcf.z()))*exp(-1.0*Cz_[3]*pow(tcf.z(), Cz_[4]));
-      
+       Su_()[cellI] = nD*(nD&fMax_)*X*Y*Z;
   }
-
-  scalar mCD=mag(CD_);
-
-  if (mCD < SMALL)
-  {
-      FatalErrorIn("Foam::fv::plasmaActuatorBodyForce::exponential::calcAxialLength() const")
-          <<"CD vector has zero length!"<<endl
-          <<abort(FatalError);
-  }
-
-  vector el=CD_/mCD;
-
-  scalar l = el & boundBox(UIndirectList<vector>(mesh_.C(), cells_)()).span();
-
-  return l;
 }
 
 
@@ -93,7 +85,9 @@ Foam::exponential::exponential
     const labelList& cells
 )
 :
-    bodyForceModel(dbd, dict, typeName, mesh, cells)
+    bodyForceModel(dbd, dict, typeName, mesh, cells),
+    Su_(new vectorField(cells.size(), pTraits<vector>::zero)),
+    correct_(true)
 {
     read(dict);
 }
@@ -111,7 +105,7 @@ bool Foam::exponential::read(const dictionary& dict)
 {
 //  if (bodyForceModel::read(dict))
 //  {
-    coeffs_.lookup("CD") >> CD_;
+    coeffs_.lookup("D") >> D_;
     coeffs_.lookup("Cx") >> Cx_;
     coeffs_.lookup("Cy") >> Cy_;
     coeffs_.lookup("Cz") >> Cz_;
@@ -130,14 +124,14 @@ bool Foam::exponential::read(const dictionary& dict)
 
 Foam::tmp<Foam::vectorField> Foam::exponential::computeSup(fvMatrix<vector>& eqn)
 {
-    tmp<vectorField> Su(new vectorField(cells_.size(), vector::zero));
+    if(correct_)
+    {
+        calcForceField();
 
-    const vectorField& U = eqn.psi();
+        correct_ = false;
+    }
 
-    Su() = 0.5*CD_* magSqr(UIndirectList<vector>(U, cells_)()) / calcAxialLength();
-    //Su() = fMax * (C1);
-
-    return Su;
+    return Su_;
 }
 
 
